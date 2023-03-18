af5dea527bbea1dad1e2e28bb07b4ce8e2ae2d53
YingXing <yingxing.cheng@ugent.be>
2023-03-18 12:03:52 +0100

    initial commit

---
+++ clean_bib.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bibtex-tidy --omit=abstract,mendeley-groups,file,annote,keywords --curly --numeric --tab --align=13 --sort=year,key --duplicates=key,doi --no-escape --no-remove-dupe-fields --output="library_cleaned.bib"  library.bib
